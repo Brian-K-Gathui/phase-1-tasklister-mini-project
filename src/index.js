6f631bc8dbdd4d9dcfb3a0e44301a91139187307
Brian <briankgathui@gmail.com>
2024-10-22 23:18:02 +0300

    Completing the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const tasksList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskDescription = document.getElementById("new-task-description").value;
+    if (taskDescription.trim() !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+      tasksList.appendChild(taskItem);
+    }
+    taskForm.reset();
+  });
 });
